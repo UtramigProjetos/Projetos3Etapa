d84ae6549c5f4824a9cabd85f2a502101712def5
270264 <270264@35169222-LAB203.utramig.local>
2014-08-19 19:54:45 -0300

    1 Versão da Cauculadora - Como vai somar os numeros

---
+++ teste/soma.java
@@ -0,0 +1,19 @@
+
+public class soma { // Soma dois Números
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int valor1;  
+		   int valor2;  
+		   int total;  
+		   valor1 = 20;  
+		   valor2 = 2;  
+		   total = valor1+valor2;  
+		   System.out.println(total);  
+		}  
+		    }  
+
+
